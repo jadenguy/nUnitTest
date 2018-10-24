05c2d4815b01a2a38001cc5baa95dea0ec1a0710
Alfredo Inostroza <jadenguy@gmail.com>
2018-10-24 12:39:19 -0400

    Making sure that DQ isn't the breaking character

--- newClass.Test/UnitTest1.cs
+++ newClass.Test/UnitTest1.cs
@@ -7,7 +7,13 @@ namespace Tests
         [Test]
         [TestCase("x.")]
         [TestCase("y.")]
+        // [TestCase("this complicates things greatly")]
         public void Test1(string message)
+        {
+            Assert.Pass();
+        }
+        [TestCase(1.1f)]
+        public void Test2(float number)
         {
             Assert.Pass();
         }
